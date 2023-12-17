8882307717c9ecfa1c183530bb6c680d5f76ea45
jhklarcher <jhklarcher@gmail.com>
2023-12-17 14:43:23 -0300

    function form

--- src/genetic-algorithm.py
+++ src/genetic-algorithm.py
@@ -12,8 +12,18 @@ def rastrigin(x):
 def rosenbrock(x):
     return sum(100.0*(x[i+1] - x[i]**2.0)**2.0 + (1 - x[i])**2.0 for i in range(len(x)-1))
 
+
+def ackley(x, a=20, b=0.2, c=2*np.pi):
+    n = len(x)
+    sum_sq_term = -a * np.exp(-b * np.sqrt(sum(xi**2 for xi in x) / n))
+    cos_term = -np.exp(sum(np.cos(c * xi) for xi in x) / n)
+    return sum_sq_term + cos_term + a + np.exp(1)
+
+
 def crossover(x1, x2, crossover_prob):
-    mask = np.random.rand(x1.shape[0]) < crossover_prob
+    if np.random.rand() > crossover_prob:
+        return x1
+    mask = np.random.rand(x1.shape[0]) < 0.5
     child = np.where(mask, x2, x1)
     return child
 
@@ -22,45 +32,43 @@ def mutation(x, bounds, mutation_prob):
     child = np.where(mask, np.random.uniform(bounds[0], bounds[1]), x)
     return child
 
-f = rosenbrock
+def ga_minimize(f, bounds, n_pop, n_gen, crossover_prob, mutation_prob):
+    pop = np.random.uniform(low=bounds[0], high=bounds[1], size=(n_pop, bounds.shape[1]))
+    fitness = [f(x) for x in pop]
+    sorted_idx = np.argsort(fitness)
+    print(f'Best of generation: {pop[sorted_idx[0]]} - {fitness[sorted_idx[0]]}')
+    history = [fitness[sorted_idx[0]]]
+    for g in range(n_gen):
+        # Crossover
+        children = np.array([
+            crossover(
+                pop[np.random.choice(pop.shape[0])],
+                pop[np.random.choice(pop.shape[0])],
+                crossover_prob=crossover_prob
+            ) for _ in range(n_pop)
+        ])
+        # Mutation
+        children = np.array([mutation(c, bounds, mutation_prob) for c in children])
+        pop = np.concatenate([pop, children])
+        fitness = np.apply_along_axis(f, 1, pop)
+        sorted_idx = np.argsort(fitness)
+        history.append(fitness[sorted_idx[0]])
+        print(f'Best of generation: {pop[sorted_idx[0]]} - {fitness[sorted_idx[0]]}')
+        pop = pop[sorted_idx[:n_pop]]
+        best = pop[0]
+        f_best = f(best)
+    return best, f_best, history
+
+
+f = ackley
 n_shape = 10
-bounds = np.array([-2*np.ones(n_shape), 2*np.ones(n_shape)])
-n_pop = 100
+bounds = np.array([-10*np.ones(n_shape), 10*np.ones(n_shape)])
+n_pop = 50
 n_gen = 1000
-crossover_prob = 0.5
+crossover_prob = 0.3
 mutation_prob = 0.1
 
-pop = np.random.uniform(low=bounds[0], high=bounds[1], size=(n_pop, bounds.shape[1]))
-fitness = [f(x) for x in pop]
-sorted_idx = np.argsort(fitness)
-print(f'Best of generation: {pop[sorted_idx[0]]} - {fitness[sorted_idx[0]]}')
-
-history = [fitness[sorted_idx[0]]]
-
-for g in range(n_gen):
-    # Crossover
-    children = np.array([
-        crossover(
-            pop[np.random.choice(pop.shape[0])],
-            pop[np.random.choice(pop.shape[0])],
-            crossover_prob=crossover_prob
-        ) for _ in range(n_pop)
-    ])
-    # Mutation
-    children = np.array([mutation(c, bounds, mutation_prob) for c in children])
-    pop = np.concatenate([pop, children])
-    fitness = np.apply_along_axis(f, 1, pop)
-    sorted_idx = np.argsort(fitness)
-    history.append(fitness[sorted_idx[0]])
-    print(f'Best of generation: {pop[sorted_idx[0]]} - {fitness[sorted_idx[0]]}')
-    pop = pop[sorted_idx[:n_pop]]
-
-x_res = pop[0]
-res =f(pop[0])
+best, f_best, history = ga_minimize(f, bounds, n_pop, n_gen, crossover_prob, mutation_prob)
+print(f'Best solution: {best} - {f_best}')
 
 plt.plot(history)
-
-x0 = bounds[1]
-pop
-
-fitness[85]
